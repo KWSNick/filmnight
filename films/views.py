277289771963192788de66ecb0284761d47c904e
Nicholas Bowley <nicholasbowley@hotmail.com>
2021-04-05 16:52:47 +0000

    Created base film card on main index.html page and added soem styling via bootstrap or base.css. Ordered cards in an overflowing, scrolling row and removed scrollbar.

--- films/views.py
+++ films/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render, redirect
+from .models import film
 
 # Create your views here.
 
 
 def index(request):
     """ A view which returns the main films page """
+
+    films = film.objects.all()
+
+    context = {
+        'films': films,
+    }
+
     if request.user.is_authenticated:
-        return render(request, 'films/index.html')
+        return render(request, 'films/index.html', context)
     else:
         return redirect('accounts/login/')
