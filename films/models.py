158178a50612800417726553dab8563d6ed73d8c
Nicholas Bowley <nicholasbowley@hotmail.com>
2021-04-05 15:53:12 +0000

    Corrected film model field names for letter casing, imported imdbtop1000amended.json fixture.

--- films/models.py
+++ films/models.py
@@ -4,28 +4,28 @@ from django.db import models
 
 
 class film(models.Model):
-    poster_link = models.URLField(max_length=2048, null=True, blank=True)
-    series_title = models.CharField(max_length=254, null=True, blank=True)
-    released_year = models.CharField(max_length=4, null=True, blank=True)
-    certificate = models.CharField(max_length=3, null=True, blank=True)
-    runtime = models.CharField(max_length=8, null=True, blank=True)
-    genre = models.CharField(max_length=20, null=True, blank=True)
-    imdb_rating = models.DecimalField(max_digits=3,
+    Poster_Link = models.URLField(max_length=2048, null=True, blank=True)
+    Series_Title = models.CharField(max_length=254, null=True, blank=True)
+    Released_Year = models.CharField(max_length=4, null=True, blank=True)
+    Certificate = models.CharField(max_length=3, null=True, blank=True)
+    Runtime = models.CharField(max_length=8, null=True, blank=True)
+    Genre = models.CharField(max_length=20, null=True, blank=True)
+    IMDB_Rating = models.DecimalField(max_digits=3,
                                       decimal_places=1,
                                       null=True,
                                       blank=True)
-    overview = models.TextField(null=True, blank=True)
-    meta_score = models.PositiveSmallIntegerField(null=True, blank=True)
-    director = models.CharField(max_length=50, null=True, blank=True)
-    star1 = models.CharField(max_length=50, null=True, blank=True)
-    star2 = models.CharField(max_length=50, null=True, blank=True)
-    star3 = models.CharField(max_length=50, null=True, blank=True)
-    star4 = models.CharField(max_length=50, null=True, blank=True)
-    no_of_votes = models.PositiveIntegerField(null=True, blank=True)
-    gross = models.CharField(max_length=50, null=True, blank=True)
+    Overview = models.TextField(null=True, blank=True)
+    Meta_score = models.PositiveSmallIntegerField(null=True, blank=True)
+    Director = models.CharField(max_length=50, null=True, blank=True)
+    Star1 = models.CharField(max_length=50, null=True, blank=True)
+    Star2 = models.CharField(max_length=50, null=True, blank=True)
+    Star3 = models.CharField(max_length=50, null=True, blank=True)
+    Star4 = models.CharField(max_length=50, null=True, blank=True)
+    No_of_Votes = models.PositiveIntegerField(null=True, blank=True)
+    Gross = models.CharField(max_length=50, null=True, blank=True)
     dvd_stock = models.IntegerField(null=True, blank=True)
     bluray_stock = models.IntegerField(null=True, blank=True)
     available = models.BooleanField(default=True, null=True, blank=True)
 
     def __str__(self):
-        return self.series_title
+        return self.Series_Title
