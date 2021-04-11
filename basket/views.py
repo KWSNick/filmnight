b66fdd373a20e0c87318d1d2b6789ed1d209c12f
Nicholas Bowley <nicholasbowley@hotmail.com>
2021-04-11 16:10:39 +0000

    Added in Bootstrap toasts with JS and Django functionality.

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render, redirect, HttpResponse, get_object_or_404
+from django.contrib import messages
 from films.models import film
 from .models import price_list
 
@@ -22,6 +23,7 @@ def view_basket(request):
 def add_to_basket(request, film_id):
     """Add films to the basket in various formats"""
 
+    Film = get_object_or_404(film, pk=film_id)
     dig_quantity = int(request.POST.get('dig_quantity'))
     dvd_quantity = int(request.POST.get('dvd_quantity'))
     br_quantity = int(request.POST.get('br_quantity'))
@@ -32,10 +34,12 @@ def add_to_basket(request, film_id):
         basket[film_id]['digital'] = 1
         basket[film_id]['dvd'] = dvd_quantity
         basket[film_id]['bluray'] = br_quantity
+        messages.success(request, f'{Film.Series_Title} Basket Updated')
     else:
         basket[film_id] = {'digital': dig_quantity,
                            'dvd': dvd_quantity,
                            'bluray': br_quantity}
+        messages.success(request, f'{Film.Series_Title} Added to Basket')
 
     request.session['basket'] = basket
     return redirect(redirect_url)
@@ -43,12 +47,14 @@ def add_to_basket(request, film_id):
 
 def remove_from(request, film_id):
     """Remove the item from the basket"""
-
+    Film = get_object_or_404(film, pk=film_id)
     try:
         basket = request.session.get('basket', {})
-        print(basket)
         basket.pop(film_id)
         request.session['basket'] = basket
+        messages.success(request, f'{Film.Series_Title} Removed Basket')
         return HttpResponse(status=200)
     except Exception as e:
+        messages.error(
+            request, f'Error removing Item {Film.Series_Title}: {e}')
         return HttpResponse(status=500)
