a5a56c5032b895f7558b0d1845bef6dad1795674
Nicholas Bowley <nicholasbowley@hotmail.com>
2021-04-11 09:56:05 +0000

    Created new html file for orderform which contains the form that sits on the filmpage.html, which is now added in an an include. switched form contents to mirror the basket table, and use the price list model from the basket.

--- films/views.py
+++ films/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import film
+from basket.models import price_list
 
 # Create your views here.
 
@@ -23,9 +24,11 @@ def filmpage(request, film_id):
     """ A view which returns the film detail page """
 
     Film = get_object_or_404(film, pk=film_id)
+    prices = price_list.objects.all()
 
     context = {
         'film': Film,
+        'prices': prices,
     }
 
     if request.user.is_authenticated:
