cb3c33690e0ebedaa225386050129754121d4bc8
Nicholas Bowley <nicholasbowley@hotmail.com>
2021-04-13 18:47:43 +0000

    Created Order model in checkout app and users model in profiles app.

--- checkout/models.py
+++ checkout/models.py
@@ -1,8 +1,48 @@
+import uuid
 from django.db import models
+from django_countries.fields import CountryField
+from profiles.models import users
 
 # Create your models here.
 
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
-    user = models.ForeignKey('profiles.users', null=False, blank=False, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        users, on_delete=models.SET_NULL, null=True, blank=True)
+    first_name = models.CharField(max_length=25, null=False, blank=False)
+    last_name = models.CharField(max_length=25, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
+    delivery_add1 = models.CharField(max_length=80, null=False, blank=False)
+    delivery_add2 = models.CharField(max_length=80, null=True, blank=True)
+    delivery_town = models.CharField(max_length=40, null=False, blank=False)
+    delivery_county = models.CharField(max_length=80, null=True, blank=True)
+    delivery_postcode = models.CharField(max_length=40, null=True, blank=True)
+    delivery_country = CountryField(
+        blank_label='Country_*', null=False, blank=False)
+    date = models.DateTimeField(auto_now_add=True)
+    delivery_charge = models.DecimalField(
+        max_digits=6, decimal_places=2, null=False, default=0)
+    order_charge = models.DecimalField(
+        max_digits=10, decimal_places=2, null=False, default=0)
+    total_charge = models.DecimalField(
+        max_digits=10, decimal_places=2, null=False, default=0)
+
+
+def order_no(self):
+    """Generates a random unique order no
+    based on CI Boutique Ado project"""
+    return uuid.uuid4().hex.upper()
+
+
+def save(self, *args, **kwargs):
+    """Override the save method so the order
+    number can be set, if it doesn't exist"""
+    if not self.order_number:
+        self.order_number = self.order_no()
+    super().save(*args, **kwargs)
+
+
+def __str__(self):
+    return self.order_number
--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from films.models import film
+from django_countries.fields import CountryField
 
 # Create your models here.
+
+
+class users(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=25, null=False, blank=False)
+    last_name = models.CharField(max_length=25, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
+    delivery_add1 = models.CharField(max_length=80, null=False, blank=False)
+    delivery_add2 = models.CharField(max_length=80, null=True, blank=True)
+    delivery_town = models.CharField(max_length=40, null=False, blank=False)
+    delivery_county = models.CharField(max_length=80, null=True, blank=True)
+    delivery_postcode = models.CharField(max_length=40, null=True, blank=True)
+    delivery_country = CountryField(
+        blank_label='Country_*', null=False, blank=False)
+    billing_add1 = models.CharField(max_length=80, null=False, blank=False)
+    billing_add2 = models.CharField(max_length=80, null=True, blank=True)
+    billing_town = models.CharField(max_length=40, null=False, blank=False)
+    billing_county = models.CharField(max_length=80, null=True, blank=True)
+    billing_postcode = models.CharField(max_length=40, null=True, blank=True)
+    billing_country = CountryField(
+        blank_label='Country_*', null=False, blank=False)
+    purchased_titles = models.ManyToManyField(film)
+
+    def __str__(self):
+        return self.user.username
