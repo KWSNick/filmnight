50ec6104a3ca4ce65d1e3192b1f1c42fd861e4da
Nicholas Bowley <nicholasbowley@hotmail.com>
2021-04-17 11:55:40 +0000

    Created check for POST method in checkout view and created form_data dict.

--- checkout/views.py
+++ checkout/views.py
@@ -15,9 +15,30 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
     basket = request.session.get('basket', {})
+
     if not basket:
         messages.error(request, 'Your shopping basket is empty')
         return redirect(reverse('films/index.html'))
+
+    if request.method == 'POST':
+        form_data = {
+            'first_name': request.POST['first_name'],
+            'last_name': request.POST['last_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'delivery_add1': request.POST['delivery_add1'],
+            'delivery_add2': request.POST['delivery_add2'],
+            'delivery_town': request.POST['delivery_town'],
+            'delivery_county': request.POST['delivery_county'],
+            'delivery_postcode': request.POST['delivery_postcode'],
+            'delivery_country': request.POST['delivery_country'],
+            'billing_add1': request.POST['billing_add1'],
+            'billing_add2': request.POST['billing_add2'],
+            'billing_town': request.POST['billing_town'],
+            'billing_county': request.POST['billing_county'],
+            'billing_postcode': request.POST['billing_postcode'],
+            'billing_country': request.POST['billing_country'],
+        }
 
     current_basket = basket_contents(request)
     total = current_basket['grand_total']
