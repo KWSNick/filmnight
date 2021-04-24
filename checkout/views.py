de55c3e850c9dfda91c6c57b446642cb564e95d6
Nicholas Bowley <nicholasbowley@hotmail.com>
2021-04-24 16:32:52 +0000

    Corrected security with checkout.views and urls.

--- checkout/views.py
+++ checkout/views.py
@@ -20,129 +20,133 @@ import json
 
 @require_POST
 def cache_checkout_data(request):
-    try:
-        pid = request.POST.get('client_secret').split('_secret')[0]
-        stripe.api_key = settings.STRIPE_SECRET_KEY
-        stripe.PaymentIntent.modify(pid, metadata={
-            'basket': json.dumps(request.session.get('basket', {})),
-            'save_delivery': request.POST.get('save_delivery'),
-            'save_billing': request.POST.get('save_billing'),
-            'username': request.user,
-        })
-        return HttpResponse(status=200)
-    except Exception as e:
-        messages.error(request,
-                       'Your payment cannot be processed, please try again later.')
-        return HttpResponse(content=e, status=400)
+    if request.user.is_authenticated:
+        try:
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            stripe.api_key = settings.STRIPE_SECRET_KEY
+            stripe.PaymentIntent.modify(pid, metadata={
+                'basket': json.dumps(request.session.get('basket', {})),
+                'save_delivery': request.POST.get('save_delivery'),
+                'save_billing': request.POST.get('save_billing'),
+                'username': request.user,
+            })
+            return HttpResponse(status=200)
+        except Exception as e:
+            messages.error(request,
+                           'Your payment cannot be processed, please try again later.')
+            return HttpResponse(content=e, status=400)
+    else:
+        return redirect(reverse('films'))
 
 
 def checkout(request):
-    stripe_public_key = settings.STRIPE_PUBLIC_KEY
-    stripe_secret_key = settings.STRIPE_SECRET_KEY
-    basket = request.session.get('basket', {})
-
-    current_basket = basket_contents(request)
-    delivery = current_basket['delivery']
-    order_total = current_basket['total']
-    total = current_basket['grand_total']
-
-    if not basket:
-        messages.error(request, 'Your shopping basket is empty')
-        if request.user.is_authenticated:
-            return redirect(reverse('films/index.html'))
-        else:
-            return redirect(reverse('films'))
-
-    if request.method == 'POST':
-        for basket_item, basket_value in current_basket.items():
-            if basket_item == 'basket_items':
-                try:
-                    for film_purchase in basket_value:
-                        film_id = film_purchase['film_id']
-                        Film = film.objects.get(id=film_id)
-                        messages.success(request, (f'{Film} Added to your collection.'))
-                except Film.DoesNotExist:
-                    messages.error(request, (
-                        "An item in your basket was not found in our database.\
-                        Please try again."
-                    ))
-
-        form_data = {
-            'first_name': request.POST['first_name'],
-            'last_name': request.POST['last_name'],
-            'email': request.POST['email'],
-            'phone_number': request.POST['phone_number'],
-            'delivery_add1': request.POST['delivery_add1'],
-            'delivery_add2': request.POST['delivery_add2'],
-            'delivery_town': request.POST['delivery_town'],
-            'delivery_county': request.POST['delivery_county'],
-            'delivery_postcode': request.POST['delivery_postcode'],
-            'delivery_country': request.POST['delivery_country'],
-            'billing_add1': request.POST['billing_add1'],
-            'billing_add2': request.POST['billing_add2'],
-            'billing_town': request.POST['billing_town'],
-            'billing_county': request.POST['billing_county'],
-            'billing_postcode': request.POST['billing_postcode'],
-            'billing_country': request.POST['billing_country'],
-        }
-        order_form = OrderForm(form_data)
-        if order_form.is_valid():
-            order = order_form.save(commit=False)
-            pid = request.POST.get('client_secret').split('_secret')[0]
-            order.stripe_pid = pid
-            order.basket = current_basket
-            order.delivery_charge = delivery
-            order.order_charge = order_total
-            order.total_charge = total
-            order.save()
-            request.session['save_delivery'] = 'save_delivery' in request.POST
-            request.session['save_billing'] = 'save_billing' in request.POST
-            return redirect(reverse('checkout_success', args=[order.order_number]))
-        else:
-            messages.error(request, "Error detected in your details.\
-                correct to process your order.")
-    else:
-        order_form = OrderForm()
-
-    stripe_total = round(total * 100)  # stripe req. integer
-    stripe.api_key = stripe_secret_key
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY,
-    )
-
-    try:
-        profile = users.objects.get(user=request.user)
-        order_form = OrderForm(initial={
-            'first_name': profile.first_name,
-            'last_name': profile.last_name,
-            'email':profile.user.email,
-            'phone_number': profile.phone_number,
-            'delivery_add1': profile.delivery_add1,
-            'delivery_add2': profile.delivery_add2,
-            'delivery_town': profile.delivery_town,
-            'delivery_county': profile.delivery_county,
-            'delivery_postcode': profile.delivery_postcode,
-            'delivery_country': profile.delivery_country,
-            'billing_add1': profile.billing_add1,
-            'billing_add2': profile.billing_add2,
-            'billing_town': profile.billing_town,
-            'billing_county': profile.first_name,
-            'billing_postcode': profile.billing_postcode,
-            'billing_country': profile.billing_country,
-        })
-    except users.DoesNotExist:
-        order_form = OrderForm()
-
-    template = 'checkout/checkout.html'
-    context = {
-        'order_form': order_form,
-        'stripe_public_key': stripe_public_key,
-        'client_secret': intent.client_secret,
-    }
-
     if request.user.is_authenticated:
+        stripe_public_key = settings.STRIPE_PUBLIC_KEY
+        stripe_secret_key = settings.STRIPE_SECRET_KEY
+        basket = request.session.get('basket', {})
+
+        current_basket = basket_contents(request)
+        delivery = current_basket['delivery']
+        order_total = current_basket['total']
+        total = current_basket['grand_total']
+
+        if not basket:
+            messages.error(request, 'Your shopping basket is empty')
+            if request.user.is_authenticated:
+                return redirect(reverse('films/index.html'))
+            else:
+                return redirect(reverse('films'))
+
+        if request.method == 'POST':
+            for basket_item, basket_value in current_basket.items():
+                if basket_item == 'basket_items':
+                    try:
+                        for film_purchase in basket_value:
+                            film_id = film_purchase['film_id']
+                            Film = film.objects.get(id=film_id)
+                            messages.success(request, (f'{Film} Added to your collection.'))
+                    except Film.DoesNotExist:
+                        messages.error(request, (
+                            "An item in your basket was not found in our database.\
+                            Please try again."
+                        ))
+
+            form_data = {
+                'first_name': request.POST['first_name'],
+                'last_name': request.POST['last_name'],
+                'email': request.POST['email'],
+                'phone_number': request.POST['phone_number'],
+                'delivery_add1': request.POST['delivery_add1'],
+                'delivery_add2': request.POST['delivery_add2'],
+                'delivery_town': request.POST['delivery_town'],
+                'delivery_county': request.POST['delivery_county'],
+                'delivery_postcode': request.POST['delivery_postcode'],
+                'delivery_country': request.POST['delivery_country'],
+                'billing_add1': request.POST['billing_add1'],
+                'billing_add2': request.POST['billing_add2'],
+                'billing_town': request.POST['billing_town'],
+                'billing_county': request.POST['billing_county'],
+                'billing_postcode': request.POST['billing_postcode'],
+                'billing_country': request.POST['billing_country'],
+            }
+            order_form = OrderForm(form_data)
+            if order_form.is_valid():
+                order = order_form.save(commit=False)
+                pid = request.POST.get('client_secret').split('_secret')[0]
+                order.stripe_pid = pid
+                order.basket = current_basket
+                order.delivery_charge = delivery
+                order.order_charge = order_total
+                order.total_charge = total
+                order.save()
+                request.session['save_delivery'] = 'save_delivery' in request.POST
+                request.session['save_billing'] = 'save_billing' in request.POST
+                return redirect(reverse('checkout_success', args=[order.order_number]))
+            else:
+                messages.error(request, "Error detected in your details.\
+                    correct to process your order.")
+        else:
+            order_form = OrderForm()
+
+        stripe_total = round(total * 100)  # stripe req. integer
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
+
+        try:
+            profile = users.objects.get(user=request.user)
+            order_form = OrderForm(initial={
+                'first_name': profile.first_name,
+                'last_name': profile.last_name,
+                'email':profile.user.email,
+                'phone_number': profile.phone_number,
+                'delivery_add1': profile.delivery_add1,
+                'delivery_add2': profile.delivery_add2,
+                'delivery_town': profile.delivery_town,
+                'delivery_county': profile.delivery_county,
+                'delivery_postcode': profile.delivery_postcode,
+                'delivery_country': profile.delivery_country,
+                'billing_add1': profile.billing_add1,
+                'billing_add2': profile.billing_add2,
+                'billing_town': profile.billing_town,
+                'billing_county': profile.first_name,
+                'billing_postcode': profile.billing_postcode,
+                'billing_country': profile.billing_country,
+            })
+        except users.DoesNotExist:
+            order_form = OrderForm()
+
+        template = 'checkout/checkout.html'
+        context = {
+            'order_form': order_form,
+            'stripe_public_key': stripe_public_key,
+            'client_secret': intent.client_secret,
+        }
+
         return render(request, template, context)
+
     else:
         return redirect(reverse('films'))
 
